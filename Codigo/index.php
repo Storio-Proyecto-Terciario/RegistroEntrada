6ab21506d596f28966032888fb9642f7f2fa1260
AcostaBruno <acostabruno706@gmail.com>
2023-10-25 09:47:43 -0300

    algo index

---
+++ Codigo/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="Vista/Css/botones.css"> 
+    <link rel="stylesheet" href="Vista/Css/formulario_1.css">  
+</head>
+<body>
+<script src="Vista/Js/restricciones_form.js"></script>
+<script src="Vista/Js/validar_login.js"></script>
+<?php
+
+if(isset($_GET['login'])){
+    if($_GET['login'] == '1'){
+        require_once('Vista/vista_entrada.html');
+    }else{
+        require_once('Vista/vista_login.html');
+    }
+}else{
+    require_once('Vista/vista_login.html');
+}
+
+
+    ?>
+</body>
+</html>
