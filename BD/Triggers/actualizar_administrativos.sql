189d9eb18b44df6b746ac3c824bacd60917950f2
Mytrcx <107264367+Martin-3519@users.noreply.github.com>
2023-11-10 18:27:39 -0300

    Delete BD directory

--- BD/Triggers/actualizar_administrativos.sql
+++
@@ -1,8 +0,0 @@
-CREATE DEFINER=`root`@`localhost` TRIGGER `actualizar_administrativos` AFTER UPDATE ON `usuarios` FOR EACH ROW BEGIN
-    if new.UsuarioExiste =0 then
-        UPDATE administrativos
-        SET AdministrativoExiste = 0
-        WHERE UsuarioCI = NEW.UsuarioCI;
-    end if;
-  
-END
--- BD/Vista/vistausuarios.sql
+++
@@ -1,9 +0,0 @@
-CREATE 
-    ALGORITHM = UNDEFINED 
-    DEFINER = root@localhost 
-    SQL SECURITY DEFINER
-VIEW proyecto.vistausuarios AS
-SELECT usuarios.*
-FROM usuarios
-LEFT JOIN administrativos ON usuarios.UsuarioCI = administrativos.UsuarioCI
-WHERE administrativos.AdministrativoExiste = false OR administrativos.UsuarioCI IS NULL;
